caa66563087d258faa10b4daf7a6f01b4a4d30c5
nav610 <nm543@cornell.edu>
2019-12-18 16:53:24 -0500

    changed grid reading, graphing scheme, and transposed matrix 12.18.19

--- 2d-grid-graph.py
+++ 2d-grid-graph.py
@@ -8,9 +8,6 @@ parser.add_argument("-grid","--grid",help="grid file to graph")
 parser.add_argument("-title","--title",help="title of plot")
 args=parser.parse_args()
 
-arr=(551,62)
-arr = np.zeros(arr)
-
 def convert_CV2(value):
     return(int(value*100))
 
@@ -24,29 +21,28 @@ for i in f.readlines()[9:]:
         pass
     else:data.append(i)
 
+previous_angle = float(data[0].split()[1])
+local=[]
+arr=[]
 for row in data:
-    #print(row.split()[0],row.split()[1])
-    CV2_index=convert_CV2(np.round(float(row.split()[0]),2))
-    CV1_index=convert_CV1(np.round(float(row.split()[1]),2))
-    bias=float(row.split()[2])
-    arr[CV2_index,CV1_index]=bias
+    angle = float(row.split()[1])
+    if angle == previous_angle:
+        local.append(float(row.split()[2]))
+    else:
+        arr.append(local)
+        previous_angle=angle
+        local=[]
+        local.append(float(row.split()[2]))
+arr =np.array(arr)* .239
+ytick = np.around(np.linspace(-3.14,3.14,62),3)
+cmap = sns.light_palette("navy")
 
-arr = arr*.239
-xtick = np.around(np.linspace(-3.14,3.14,62),3)
-ytick = np.around(np.linspace(0,5.5,552),3)
-
-#plt.figure(figsize=(10,10))
-graph = sns.heatmap(arr,vmin=np.amin(arr),vmax=np.amax(arr),xticklabels=xtick,
-    yticklabels=ytick)
-for ind,label in enumerate(graph.get_xticklabels()):
-    if ind%10 == 0:
-        label.set_visible(True)
-    else: label.set_visible(False)
+graph = sns.heatmap(arr,vmin=np.amin(arr),vmax=np.amax(arr),
+    yticklabels=ytick,cmap=cmap)
 for ind,label in enumerate(graph.get_yticklabels()):
-    if ind%50 == 0:
+    if ind%5 == 0:
         label.set_visible(True)
     else: label.set_visible(False)
-graph.set(xlabel='phi [rad]',ylabel='d [nm]')
 
-
+graph.set(ylabel='phi [rad]',xlabel='d [mE-11]')
 plt.show()
