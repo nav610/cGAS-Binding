4e36b9c8f666c0c805c8b19845820bd4e02d4663
nav610 <nm543@cornell.edu>
2020-03-02 12:02:34 -0500

    03.02.20 findXYZ and find COM

--- 4k98-init-CVs.py
+++ 4k98-init-CVs.py
@@ -1,11 +1,26 @@
 import mdtraj as md
+import numpy as np
 
 t = md.load("4k98.pdb")
-top = t.topolgy()
-lysCA = top.select("resSeq 409 and name CA")
-serCB = top.select("resSeq 305 and name CB")
-mg = top.select("name MG")
-cat = top.select("resSeq 213 and name CA")
+top = t.topology
+
+anchors = top.select("resSeq 409 and name CA or resSeq 305 and name CB")
+catRes = top.select("name MG or resSeq215 and name CA")
 PA = top.select("resSeq 604 and name PA")
 ring1 = top.select("resSeq 604 and name C12 or name C22 or name C32 or name C42 or name O42")
 ring2 = top.select("resSeq 605 and name C1'1 or name C2'1 or name C3'1 or name C4'1 or name O4'1")
+
+anchors_mass = np.array([12.0110,12.0110])
+catRes_mass = np.array([24.3050,24.3050,12.0110])
+ring1_mass = np.array([12.0110,12.0110,12.0110,12.0110,15.9994])
+ring2_mass = np.array()[12.0110,12.0110,12.0110,12.0110,15.9994])
+
+
+def findXYZ(index,trajxyz):
+    cords=[]
+    for i in index:
+        coords.append(trajxyz[i])
+    return np.array(cords)
+
+def findCOM(massArr,coordArr):
+    return(np.array(massArr,coordArr)/massArr)
